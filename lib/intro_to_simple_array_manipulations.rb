92408f44d255e7159b56e98d28ed5bde9e8e99fc
Orlando Challenger <orlandofat@gmail.com>
2020-05-09 23:03:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat (array1, array2)
 end
 
 def using_insert (array, element)
-  array.insert(array.size/4, "Python")
+  array.insert(array.size/7, "Python")
 end
 
 def using_uniq (array)
